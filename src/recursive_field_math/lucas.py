1808ebb6d728685cde8e002d5e8c15d76666f133
wizardaax <adzextreem@gmail.com>
2025-08-22 18:39:52 -0700

    Create lucas.py

---
+++ src/recursive_field_math/lucas.py
@@ -0,0 +1,7 @@
+from .constants import PHI, PSI
+
+def L(n: int) -> int:
+    """Lucas number via closed form."""
+    if n < 0:
+        raise ValueError("n must be >= 0")
+    return int(round(PHI**n + PSI**n))
