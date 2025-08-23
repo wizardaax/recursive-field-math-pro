dfa2849e88c53653399c1a405870c1de59270ace
wizardaax <adzextreem@gmail.com>
2025-08-22 18:43:47 -0700

    Create egyptian_fraction.py

---
+++ src/recursive_field_math/egyptian_fraction.py
@@ -0,0 +1,5 @@
+def egypt_4_7_11():
+    """Return exact rational sum for 1/4 + 1/7 + 1/11 = 149/308."""
+    from fractions import Fraction
+    s = Fraction(1,4) + Fraction(1,7) + Fraction(1,11)
+    return s.numerator, s.denominator
